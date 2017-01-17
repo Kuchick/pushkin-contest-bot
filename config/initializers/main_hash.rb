74ffb24170a5e43b446dac146127cb709d668ce5
Kuchick <kuchinskiysergeysergeevich@gmail.com>
2017-01-17 07:54:02 +0300

    1

--- config/initializers/main_hash.rb
+++ config/initializers/main_hash.rb
@@ -5,6 +5,10 @@ $main_hash = {}
 
 raw_hash.each do |arr|
   arr[1].each do |string_of_poem|
+  	string_of_poem.gsub!(',', ' ')
+  	string_of_poem.gsub!('.', ' ')
+  	string_of_poem.gsub!('  ', ' ')
+  	string_of_poem.strip!
     $main_hash[string_of_poem] = arr[0]
   end
 end
