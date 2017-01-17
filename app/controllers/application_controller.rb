0904d998b6548cc2f1d8f5dcc4ebd069ff128e0a
Kuchick <kuchinskiysergeysergeevich@gmail.com>
2017-01-17 06:37:14 +0300

    Set up Puma, Nginx & Capistrano v1000

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,33 @@ class ApplicationController < ActionController::Base
 
   def index
   end
+
+  def quiz
+    question = params[:question]
+    task_id = params[:task_id]
+    level = params[:level]
+    answer = $main_hash[question]
+
+    send_answer(answer, task_id)
+  end
+
+  def send_answer(answer, task_id)
+    uri = URI('http://pushkin.rubyroidlabs.com/quiz')
+    parameters = {
+      answer: answer,
+      token: '86f13a0f77fc65a43feef10900c5a721',
+      task_id:  task_id
+    }
+    Net::HTTP.post_form(uri, parameters) 
+  end
+
+  def first_task
+  
+    
+  end
+
   
 end
+
+
+
---
+++ config/initializers/main_hash.rb
@@ -0,0 +1,12 @@
+require 'json'
+
+file = File.open("poems-full.json")
+content = file.read
+raw_hash = JSON.parse(content)
+$main_hash = {}
+
+raw_hash.each do |arr|
+  arr[1].each do |string_of_poem|
+    $main_hash[string_of_poem] = arr[0]
+  end
+end
